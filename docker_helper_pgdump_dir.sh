805f115ec977a746974e5494ba5fdd77ecbffdef
Aleksandr <aleksandr.ivin@ibuy.ua>
2019-10-03 16:01:47 +0200

    Added pd_dump mutijobs

---
+++ docker_helper_pgdump_dir.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+NETWORK=$1
+CONTAINER_NAME=$2
+DB_USER=$3
+DB_PASSWORD=$4
+DB_NAME=$5
+PG_VERSION=$6
+PG_DUMP_OPTIONS=$7
+
+if [ -z $NETWORK ];
+then
+   echo 'EMPTY NETWORK'
+   exit 1
+fi
+
+if [ -z $CONTAINER_NAME ];
+then
+   echo 'EMPTY CONTAINER_NAME'
+   exit 1
+fi
+
+if [ -z $DB_PASSWORD ];
+then
+   echo 'EMPTY DB_USER'
+   exit 1
+fi
+
+if [ -z $DB_NAME ];
+then
+   echo 'EMPTY DB_NAME'
+   exit 1
+fi
+
+#docker volume ls -q | grep -x $1
+#if [ $? -ne 0 ];
+#then
+#   echo -e "\033[00;31m ERROR! Volume $1 not found \033[0m"
+#   echo "List of docker volumes: "
+#   docker volume ls -q | grep $1i
+#   exit 1
+#fi
+
+# exit 0
+
+CURRENT_DATETIME=$(date '+%Y-%m-%d_%H-%M-%S')
+TMP_DUMP_DIR="tmp_dump_$CURRENT_DATETIME"
+#EXPORT_DIR="${CONTAINER_NAME}-${CURRENT_DATETIME}"
+#mkdir ${EXPORT_DIR}
+
+docker run --rm -it -v `pwd`:/tmp/export/data \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    --link ${CONTAINER_NAME}:dbhost \
+    --network ${NETWORK} \
+    postgres:${PG_VERSION} \
+    /bin/bash -c "export PGPASSWORD=${DB_PASSWORD} && cd /tmp/export/data \
+        && mkdir -p $TMP_DUMP_DIR \
+        && pg_dump -h dbhost -U ${DB_USER} ${DB_NAME} ${PG_DUMP_OPTIONS} -F d -f $TMP_DUMP_DIR \
+        && tar -cvf ${CONTAINER_NAME}-${DB_NAME}-${CURRENT_DATETIME}.tar.gz $TMP_DUMP_DIR \
+        && chmod 0766 ${CONTAINER_NAME}-${DB_NAME}-${CURRENT_DATETIME}.tar.gz \
+        && rm -r $TMP_DUMP_DIR"
+
+printf "${NETWORK}\n${CONTAINER_NAME}\n${CONTAINER_NAME}-${DB_NAME}-${CURRENT_DATETIME}.tar.gz\n" >> ${CONTAINER_NAME}.txt
+
--- make_links.sh
+++ make_links.sh
@@ -1,4 +1,5 @@
 sudo ln -s `pwd`/docker_helper_pgdump.sh /usr/local/bin/docker-helper-pgdump
+sudo ln -s `pwd`/docker_helper_pgdump_dir.sh /usr/local/bin/docker-helper-pgdump-dir
 sudo ln -s `pwd`/docker_helper_extract_volume.sh /usr/local/bin/docker-helper-extract-volume
 sudo ln -s `pwd`/docker_helper_pgrestore.sh /usr/local/bin/docker-helper-pgrestore
 
